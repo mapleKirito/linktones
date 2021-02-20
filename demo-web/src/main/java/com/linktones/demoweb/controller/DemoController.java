9287d8eb435fe59620fc0be95a2598e70cb4c559
maple <myzhouye@gmail.com>
2021-02-20 09:28:09 +0800

    2021年2月19日，添加测试jenkins打包项目

---
+++ demo-web/src/main/java/com/linktones/demoweb/controller/DemoController.java
@@ -0,0 +1,40 @@
+package com.linktones.demoweb.controller;
+/***
+ *                    _ooOoo_
+ *                   o8888888o
+ *                   88" . "88
+ *                   (| -_- |)
+ *                    O\ = /O
+ *                ____/`---'\____
+ *              .   ' \\| |// `.
+ *               / \\||| : |||// \
+ *             / _||||| -:- |||||- \
+ *               | | \\\ - /// | |
+ *             | \_| ''\---/'' | |
+ *              \ .-\__ `-` ___/-. /
+ *           ___`. .' /--.--\ `. . __
+ *        ."" '< `.___\_<|>_/___.' >'"".
+ *       | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *         \ \ `-. \_ __\ /__ _/ .-` / /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ *                    `=---='
+ *
+ * .............................................
+ *          佛祖保佑             永无BUG
+ */
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: 那条蠢鱼
+ * @Date: 2021/2/20 09:23
+ * @Description: # 描述
+ */
+@RestController
+public class DemoController {
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello";
+    }
+}
